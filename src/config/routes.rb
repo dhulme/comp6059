31a2b2634a5b908db4a90c581c2b2ded102cbdd0
Dave Hulme <>
2013-10-08 15:48:59 +0100

    Added stuff

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,10 +1,10 @@
 Src::Application.routes.draw do
-  get "home/index"
+  get "discover/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'discover#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'x
@@ -13,7 +13,7 @@ Src::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+ resources :templates
 
   # Example resource route with options:
   #   resources :products do
