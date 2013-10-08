d6e0fe9a0b551e27e970426a6bddf7f1844b3389
Dave Hulme <>
2013-10-08 16:34:07 +0100

    Added templates, started implementing CRUD methods

--- src/app/controllers/templates_controller.rb
+++ src/app/controllers/templates_controller.rb
@@ -1,2 +1,9 @@
 class TemplatesController < ApplicationController
+  def new
+   #uploaded_io = params[:template][:picture]
+    #File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
+   #   file.write(uploaded_io.read)
+   # end
+   @template = Template.new
+  end
 end
