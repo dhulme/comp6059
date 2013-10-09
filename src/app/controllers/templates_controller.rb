72796900212a610c551362bd74526930b92cdfe3
Dave Hulme <>
2013-10-09 16:33:27 +0100

    You can now upload files

--- src/app/controllers/templates_controller.rb
+++ src/app/controllers/templates_controller.rb
@@ -1,9 +1,13 @@
 class TemplatesController < ApplicationController
   def new
-   #uploaded_io = params[:template][:picture]
-    #File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
-   #   file.write(uploaded_io.read)
-   # end
    @template = Template.new
+  end
+  
+  def create
+    logger.info 'uploading'
+    uploaded_io = params[:template][:picture]
+    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
   end
 end
