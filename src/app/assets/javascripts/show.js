b8c02297024492b38f25db3c0718ebf03e29cebb
dhulme <>
2014-01-01 16:44:24 +0000

    Bug fixes

--- src/app/assets/javascripts/show.js
+++ src/app/assets/javascripts/show.js
@@ -69,7 +69,7 @@ $(function() {
         + date.getHours() + ':' + date.getMinutes();
       
       // Add review HTML
-      var html = '<blockquote><p>' + res.comment + '</p>' + starsHTML + '<small>AUTHOR, at ' + timeHTML + '</small></blockquote>';
+      var html = '<blockquote><p>' + res.comment + '</p>' + starsHTML + '<small>You, at ' + timeHTML + '</small></blockquote>';
 
       $('#reviews').prepend(html);
     });
